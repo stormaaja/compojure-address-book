aac6abf1bf48b784dc4bfd046c77ee0302af413c
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-18 22:49:47 +0300

    Update to use wiite

--- test/address_book/core/address_book_tests.clj
+++ test/address_book/core/address_book_tests.clj
@@ -3,35 +3,40 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [address-book.core.handler :refer :all]
-            [address-book.core.models.query-defs :as query]))
+            [clj-wiite.core :as w]
+            [environ.core :refer [env]]))
 
 (facts "Example GET and POST tests"
-  (with-state-changes [(before :facts (query/create-contacts-table-if-not-exists!))
-                       (after  :facts (query/drop-contacts-table!))]
+  (with-state-changes [(before :facts (reset! (w/watom (env :db-connection)) []))
+                       (after  :facts (reset! (w/watom (env :db-connection)) []))]
 
   (fact "Test GET request to / route returns expected contacts"
-    (query/insert-contact<! {:name "JT" :phone "(321)" :email "JT@JT.com"})
-    (query/insert-contact<! {:name "Utah" :phone "(432)" :email "J@Buckeyes.com"})
+    (let [watom (w/watom (env :db-connection))]
+      (reset! watom [{:id 1 :name "JT" :phone "(321)" :email "JT@JT.com"}
+                     {:id 2 :name "Utah" :phone "(432)" :email "J@Buckeyes.com"}]))
     (let [response (app (mock/request :get "/"))]
       (:status response) => 200
       (:body response) => (contains "<div class=\"column-1\">JT</div>")
       (:body response) => (contains "<div class=\"column-1\">Utah</div>")))
 
   (fact "Test POST to /post creates a new contact"
-    (count (query/all-contacts)) => 0
+    (count (deref (w/watom (env :db-connection)))) => 0
     (let [response (app (mock/request :post "/post" {:name "Some Guy" :phone "(123)" :email "a@a.cim"}))]
       (:status response) => 302
-      (count (query/all-contacts)) => 1))
+      (count (deref (w/watom (env :db-connection)))) => 1))
 
   (fact "Test UPDATE a post request to /edit/<contact-id> updates desired contact information"
-    (query/insert-contact<! {:name "JT" :phone "(321)" :email "JT@JT.com"})
+    (let [watom (w/watom (env :db-connection))]
+      (reset! watom [{:id 1 :name "JT" :phone "(321)" :email "JT@JT.com"}]))
     (let [response (app (mock/request :post "/edit/1" {:id "1" :name "Jrock" :phone "(999) 888-7777" :email "jrock@test.com"}))]
       (:status response) => 302
-      (count (query/all-contacts)) => 1
-      (first (query/all-contacts)) => {:id 1 :name "Jrock" :phone "(999) 888-7777" :email "jrock@test.com"}))
+      (let [watom (w/watom (env :db-connection))]
+        (count @watom) => 1
+        (first @watom) => {:id 1 :name "Jrock" :phone "(999) 888-7777" :email "jrock@test.com"})))
 
     (fact "Test DELETED a post to /delete/<contact-id> deletes desired contact from database"
-      (query/insert-contact<! {:name "JT" :phone "(321)" :email "JT@JT.com"})
-      (count (query/all-contacts)) => 1
+      (let [watom (w/watom (env :db-connection))]
+        (reset! watom [{:id 1 :name "JT" :phone "(321)" :email "JT@JT.com"}])
+        (count @watom) => 1)
       (let [response (app (mock/request :post "/delete/1" {:id 1}))]
-        (count (query/all-contacts)) => 0))))
+        (count (deref (w/watom (env :db-connection)))) => 0))))

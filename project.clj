302f907ffbdab96c96eccae666fbd67a2360dac4
Matti Ahinko <matti.ahinko@gmail.com>
2018-06-18 22:47:55 +0300

    Update packages and add wiite

--- project.clj
+++ project.clj
@@ -6,23 +6,21 @@
   :ring {:handler address-book.core.handler/app
          :init    address-book.core.handler/init}
 
-  :dependencies   [[org.clojure/clojure   "1.6.0"]
-                   [compojure             "1.3.1"]
-                   [ring/ring-defaults    "0.1.3"]
-                   [hiccup                "1.0.5"]
-                   [org.clojure/java.jdbc "0.3.6"]
-                   [postgresql/postgresql "9.3-1102.jdbc41"]
-                   [yesql                 "0.5.0-rc1"]
-                   [environ               "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure             "1.6.1"]
+                 [ring/ring-defaults    "0.3.2"]
+                 [hiccup                "1.0.5"]
+                 [environ               "1.1.0"]
+                 [clj-wiite "0.1.0"]]
 
-  :plugins        [[lein-ring             "0.9.1"]
-                   [lein-environ          "1.0.0"]]
+  :plugins        [[lein-ring             "0.12.4"]
+                   [lein-environ          "1.1.0"]]
 
-  :profiles {:test-local {:dependencies [[midje "1.6.3"]
+  :profiles {:test-local {:dependencies [[midje "1.9.1"]
                                          [javax.servlet/servlet-api "2.5"]
-                                         [ring-mock "0.1.5"]]
+                                         [ring/ring-mock "0.3.2"]]
 
-                           :plugins     [[lein-midje "3.1.3"]]}
+                          :plugins     [[lein-midje "3.2.1"]]}
 
              ;; Set these in ./profiles.clj
              :test-env-vars {}
